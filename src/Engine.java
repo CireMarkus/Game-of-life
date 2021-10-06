a940293965e6d700a84e323a9430e2a1fc6888bc
Eric Washington <Mrkimjarrow95@gmail.com>
2021-10-06 17:09:28 -0500

    Created a JFrame and added a Jlabel with text. Don't @ me.

--- src/Engine.java
+++ src/Engine.java
@@ -1,7 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Engine {
     
     public static void main(String[] args) {
         //TODO:Implement the JFrame
+        
+        JFrame mainFrame = new JFrame("Eric's Game of Life");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        mainFrame.setSize(500,500);
+
+        mainFrame.getContentPane().add(new JLabel("Destroy all Humans",SwingConstants.CENTER));
+        mainFrame.setVisible(true);
 
     }
     
