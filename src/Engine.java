4da32042e222fbfd8a8d6af130696224203f2c07
Eric Washington <Mrkimjarrow95@gmail.com>
2021-10-07 22:19:13 -0500

    Completed the JFrame and started work on the main menu.

--- src/Engine.java
+++ src/Engine.java
@@ -1,18 +1,24 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Engine {
     
     public static void main(String[] args) {
-        //TODO:Implement the JFrame
+        
         
         JFrame mainFrame = new JFrame("Eric's Game of Life");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         mainFrame.setSize(500,500);
 
-        mainFrame.getContentPane().add(new JLabel("Destroy all Humans",SwingConstants.CENTER));
+        JPanel mainMenu = new JPanel();
+
+        //TODO: Create the main menu for the game.
+        mainFrame.add(mainMenu);
+
+
         mainFrame.setVisible(true);
+
+        
 
     }
     
